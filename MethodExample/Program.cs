503cca09f5c7e30139a9acc2c76aaa3e48fdfa8f
Tom Nagle <tnagle10@gmail.com>
2016-10-27 11:57:50 -0400

    Initial Program

--- MethodExample/Program.cs
+++ MethodExample/Program.cs
@@ -8,8 +8,31 @@ namespace MethodExample
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Please enter min and max values.  I will sum all numbers in between");
+            Console.WriteLine("Enter a minimum number");
+            int min = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a maximum number");
+            int max = int.Parse(Console.ReadLine());
+            int sumMinMax = sumMinToMax(min, max);
+            Console.WriteLine("Here is the sum:  " + sumMinMax);
+        }
+
+
+        static int sumMinToMax(int min, int max)
+        {
+            int sum = 0;
+            for (int i = min; i <= max; i++)
+            {
+                sum = sum + i;
+                Console.WriteLine("I am summing them up " + sum);
+
+            }
+            return sum;
         }
     }
 }
